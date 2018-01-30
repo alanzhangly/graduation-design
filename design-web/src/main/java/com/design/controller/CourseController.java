e7f59f5a5c21ab45332fafa0e446dd769e21ee59
ke Zhang <974077194@qq.com>
2018-01-30 10:58:34 +0800

    1

---
+++ design-web/src/main/java/com/design/controller/CourseController.java
@@ -0,0 +1,12 @@
+package com.design.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Ke Zhang on 2018/1/30.
+ *
+ * 选课接口
+ */
+@RestController
+public class CourseController {
+}
---
+++ design-web/src/main/java/com/design/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.design.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Ke Zhang on 2018/1/30.
+ */
+@RestController
+public class StudentController {
+}
